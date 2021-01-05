59107b46f4900225391c39e70c7771eff22fb5c9
Gaddam Ri <ri@CoolGuy5583.attlocal.net>
2021-01-05 07:28:42 -0600

    account-page

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, IntegerField, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from Google import Create_Service
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '792322cf65e1fabb70b75e0b926750b4158940f43bcde2f3a93ab717aec5df8324b11ec53b123164cb51669b51004b3af73899b32c75d8679e0b518d68192aa1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+app                                     = Flask(__name__)
+app.config['SECRET_KEY']                = '792322cf65e1fabb70b75e0b926750b4158940f43bcde2f3a93ab717aec5df8324b11ec53b123164cb51669b51004b3af73899b32c75d8679e0b518d68192aa1'
+app.config['SQLALCHEMY_DATABASE_URI']   = 'sqlite:///site.db'
+db                                      = SQLAlchemy(app)
+bcrypt                                  = Bcrypt(app)
+login_manager                           = LoginManager(app)
+login_manager.login_view = 'login'
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -21,7 +25,6 @@ class User(db.Model, UserMixin):
     id          = db.Column(db.Integer, primary_key=True)
     first_name  = db.Column(db.String(30), nullable=True)
     last_name   = db.Column(db.String(30), nullable=True)
-    gender      = db.Column(db.String(5), nullable=True)
     grade       = db.Column(db.Integer, nullable=False)
     username    = db.Column(db.String(30), unique=True, nullable=False)
     email       = db.Column(db.String(120), unique=True, nullable=False)
@@ -34,7 +37,6 @@ class SignUpForm(FlaskForm):
     first_name          = StringField("First name: ", validators=[DataRequired()])
     last_name           = StringField("Last name: ", validators=[DataRequired()])
     grade               = IntegerField("Grade: ", validators=[DataRequired()])
-    gender              = RadioField("Gender: ", validators=[DataRequired()], choices=['Male', 'Female'])
     username            = StringField("Username: ", validators=[DataRequired(), Length(min=3, max=30)])
     email               = StringField("Email: ", validators=[DataRequired(), Email()])
     password            = PasswordField("Password: ", validators=[DataRequired()])
@@ -56,6 +58,8 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password: ", validators=[DataRequired()])
     remember = BooleanField('Remember Me: ')
     submit = SubmitField('Login')
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
 @app.route('/')
 @app.route('/home')
@@ -85,24 +89,27 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login unsuccessful. Either the email or password is wrong.')
     return render_template('login.html', form=form, showNav=True, title='GTG - Login')
 
 @app.route('/account')
+@login_required
 def account():
-    return '<h1>In Development</h1>'
+    return render_template('account.html', showNav=True, title='GTG-Account')
 
 @app.route('/gpa')
 def calculator():
-    return render_template('calculator.html')
+    return render_template('calculator.html', title='GTG - GPA Calculator')
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
 
 if __name__ == '__main__':
     app.run(debug=True)
